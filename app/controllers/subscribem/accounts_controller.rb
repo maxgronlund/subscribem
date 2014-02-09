c21a242505a81fee6b38e7b6caa420e38a60f425
Synthmax <max@synthmax.dk>
2014-02-09 23:33:37 +0100

    signing in working, move on to 2.7

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -8,12 +8,12 @@ module Subscribem
     end
     
     def create
-      @account = Subscribem::Account.new(account_params)
-      if @account.save
-        env["warden"].set_user(@account.owner, :scope => :user)
-        env["warden"].set_user(@account, :scope => :account)
+      account = Subscribem::Account.create_with_owner(account_params)
+      if account.valid?
+        env["warden"].set_user(account.owner, :scope => :user)
+        env["warden"].set_user(account.id, :scope => :account)
         flash[:success] = "Your account has been successfully created."
-        redirect_to subscribem.root_url(:subdomain => @account.subdomain)
+        redirect_to subscribem.root_url(:subdomain => account.subdomain)
       else
         flash[:error] = "Sorry, your account could not be created."
         render :new
--- config/initializers/warden/strategies/password.rb
+++ config/initializers/warden/strategies/password.rb
@@ -1,16 +1,34 @@
+#Warden::Strategies.add(:password) do
+#  
+#  def valid?
+#    host = request.host
+#    subdomain = ActionDispatch::Http::URL.extract_subdomains(host, 1)
+#    subdomain.present? && params["user"]
+#  end
+#  
+#  def authenticate!
+#    if u = Subscribem::User.find_by(email: params["user"]["email"])
+#      u.authenticate(params["user"]["password"]) ? success!(u) : fail!
+#    else
+#      fail!
+#    end
+#  end
+#end
+
 Warden::Strategies.add(:password) do
   
-  def valid?
-    host = request.host
-    subdomain = ActionDispatch::Http::URL.extract_subdomains(host, 1)
-    subdomain.present? && params["user"]
+  def subdomain
+    ActionDispatch::Http::URL.extract_subdomains(request.host, 1)
   end
   
+  def valid?
+    subdomain.present? && params["user"]
+  end
+
   def authenticate!
-    if u = Subscribem::User.find_by(email: params["user"]["email"])
-      u.authenticate(params["user"]["password"]) ? success!(u) : fail!
-    else
-      fail!
-    end
+    return fail! unless account = Subscribem::Account.find_by(subdomain: subdomain)
+    return fail! unless user = account.users.find_by(email: params["user"]["email"])
+    return fail! unless user.authenticate(params["user"]["password"])
+    success! user
   end
 end
