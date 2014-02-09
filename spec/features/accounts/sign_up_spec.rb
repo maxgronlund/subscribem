a349eea193ba6d1ae8a706f9e72cb615564d6766
Synthmax <max@synthmax.dk>
2014-02-09 19:52:10 +0100

    owner for the account being associated

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -12,5 +12,6 @@ feature "Accounts" do
     click_button "Create Account"
     success_message = "Your account has been successfully created."
     page.should have_content(success_message)
+    page.should have_content("Signed in as subscribem@example.com")
   end
 end
